bb3e108ac3df8af9abf78b7c0f0a3b0a1b3ccd88
cjttkfkd3941 <35517998+cjttkfkd3941@users.noreply.github.com>
2019-05-26 10:54:08 +0900

    Update stkque.py

--- stkque.py
+++ stkque.py
@@ -24,9 +24,12 @@ class StackNode:
         top =  self.top.data
         if top==None:
             pass
+        elif self.top.next==self:
+            self.top = None
+            self.size -= 1
         else:
             self.top = self.top.next
-        self.size -= 1
+            self.size -= 1
         return top
     
     def peek(self):
@@ -83,7 +86,7 @@ class QueNode():
     def peek(self):
         n = self
         out = n.head
-        return out.data
+        return out
 
     def isEmpty(self):
         if not(self.head) and not(self.tail):
@@ -140,3 +143,238 @@ class SetOfStacks():
             return True
         else:
             return False
+
+# 3.4
+class MyQueue():
+    def __init__(self):
+        self.stack1 = StackNode()
+        self.stack2 = StackNode()
+        self.switch = 1
+        #switch==1이면 stack1에 다 들어가있는거, 0이면 2에 들어가는거로 정의
+    def push(self,data):
+        if self.switch == 1:
+            pass
+        else:
+            self.moving(self.stack2,self.stack1)
+            self.switch = 1
+        self.stack1.push(data)
+    
+    def pop(self):
+        if self.switch == 1:
+            self.moving(self.stack1,self.stack2)
+            self.switch = 0
+        else:
+            pass
+        return self.stack2.pop()
+
+    def moving(self,stack1,stack2):
+        while stack1.top:
+            stack2.push(stack1.pop())
+
+        
+    def isEmpty(self):
+        if self.stack1.isEmpty() and self.stack2.isEmpty():
+            return True
+        else:
+            return False
+            
+def StackSort(stack1):##size,min 부분은 고려안하고 next,pop,push,top개념만 고려
+    s = stack1
+    s2 = StackNode()
+    while s.top:
+        top = s.top #top은 가장 위의 노드
+        s.top = s.top.next
+        top2 = s2.top#top2 는 소팅된 가장 위 노드
+        if top2==None:
+            top.next = s2
+            s2.top = top
+        else:
+            if top.data<top2.data:
+                top.next = s2.top
+                s2.top = top
+            else:
+                while top2.next.data:  
+                    if top.data<top2.next.data:
+                        break
+                    else:
+                        top2 = top2.next
+                top.next = top2.next
+                top2.next = top
+    return s2
+
+class shelter():
+    count = 0
+    def __init__(self):
+        self.QueCat = QueNode()
+        self.QueDog = QueNode()
+    def push(self,species):
+        if species=='cat'or species=="Cat":
+            self.QueCat.add(self.count+1)
+        if species=="dog" or species=="Dog":
+            self.QueDog.add(self.count+1)
+        shelter.increment()
+    def popCat(self):
+        cat = self.QueCat.remove()
+        print("Cat",end=" ")
+        return cat
+    def popDog(self):
+        dog = self.QueDog.remove()
+        print("Dog",end=" ")
+        return dog
+    def popAny(self):
+        c = self.QueCat.peek()
+        d = self.QueDog.peek()
+        if c==None:
+            c = QueNode(self.count+1)
+        if d==None:
+            d = QueNode(self.count+1)
+        if c.data<d.data:
+            print("Cat",end=" ")
+            return self.QueCat.remove()
+        elif c==d:
+            return "Don't have Animal"
+        else:
+            print("Dog",end=" ")
+            return self.QueDog.remove()
+
+    @classmethod
+    def increment(cls):
+        cls.count += 1
+    
+
+#####################################################################################
+#from stkque import *
+
+#3.2
+'''
+a= StackNode()
+a.push(1)
+print(StackMin(a))
+a.push(2)
+print(StackMin(a))
+a.push(5)
+print(StackMin(a))
+a.push(10)
+print(StackMin(a))
+a.push(-3)
+print(StackMin(a))
+a.get()
+print(a.peek())
+a.pop()
+print(a.peek())
+print(StackMin(a))
+a.pop()
+print(a.peek())
+a.pop()
+print(a.isEmpty())
+
+'''
+
+
+'''
+a= QueNode()
+a.add(1)
+a.add(2)
+a.add(5)
+a.get()
+print(a.peek())
+a.remove()
+print(a.peek())
+a.remove()
+print(a.peek())
+a.remove
+print(a.isEmpty())
+'''
+
+
+#3.3
+'''
+a = SetOfStacks()
+a.push(3)
+a.push(5)
+a.push(10)
+a.push(15)
+a.push(20)
+a.push(17)
+a.push(13)
+print(a.peek())
+print(a.pop())
+print(a.peek())
+print(a.pop())
+print(a.peek())
+print(a.pop())
+print(a.peek())
+print(a.pop())
+print(a.peek())
+print(a.pop())
+print(a.peek())
+print(a.pop())
+print(a.peek())
+print(a.pop())
+print(a.isEmpty())
+'''
+
+#3.4
+'''
+a = MyQueue()
+a.push(3)
+a.push(5)
+a.push(10)
+a.push(8)
+a.push(20)
+a.push(17)
+a.push(13)
+print(a.pop())
+a.push(100)
+print(a.pop())
+print(a.pop())
+print(a.pop())
+print(a.pop())
+a.push(200)
+print(a.pop())
+print(a.pop())
+print(a.pop())
+print(a.pop())
+print(a.isEmpty())
+'''
+
+#3.5
+'''
+a = StackNode()
+a.push(3)
+a.push(5)
+a.push(10)
+a.push(8)
+a.push(20)
+a.push(17)
+a.push(13)
+b = StackSort(a)
+print(b.pop())
+print(b.pop())
+print(b.pop())
+print(b.pop())
+print(b.pop())
+print(b.pop())
+print(b.pop())
+print(b.isEmpty())
+'''
+#3.6
+'''
+a = shelter()
+a.push("cat")#1
+a.push("cat")#2
+a.push("dog")#3
+a.push("cat")#4
+a.push("cat")#5
+a.push("cat")#6
+a.push("dog")#7
+a.push("dog")#8
+print(a.popCat())
+print(a.popCat())
+print(a.popCat())
+print(a.popAny())
+print(a.popDog())
+print(a.popAny())
+print(a.popDog())
+print(a.popAny())
+'''
