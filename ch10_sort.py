164d41ab430e685396a21d44e825b979d65d32f4
cjttkfkd3941 <35517998+cjttkfkd3941@users.noreply.github.com>
2019-08-17 10:49:47 +0900

    Update ch10_sort.py

--- ch10_sort.py
+++ ch10_sort.py
@@ -50,9 +50,7 @@ def quick_sort(l=list()):
     res = [num]
     res = s_left+res+s_right
     return res
-
 '''
-#요구조건 충족 못하는 코드
 def func1(l1,l2):
     l = []
     while l1 and l2:
@@ -65,7 +63,6 @@ def func1(l1,l2):
     l += l1+l2
     return l
     '''
-
 
 def func1(a,b):#수정변경
     a_point,b_point = len(a)-len(b)-1,len(b)-1
@@ -83,11 +80,11 @@ def func1(a,b):#수정변경
     a = b[:b_point+1]+a[-t_point:]
     return a
 
-def func2(strings=['abc']):#깔끔하게 푸는법을 모르겠음...
+def func2(strings=['abc']):#깔끔하게 푸는법을 모르겠드아...
     table = []
     buffer = []
     for string in strings:
-        value = "".join(map(chr,merge_sort(list(map(ord,string)))))
+        value = "".join(map(chr,merge_sort(list(map(ord,string)))))#복잡도 개쓰레기ㅠㅠ
         if value not in table:
             table.append(value)
             buffer.append([string])
@@ -183,15 +180,269 @@ def func4(l=Listy(),x=0):
         elif k>x:
             last = n
             n = (start+last)//2
+
+def func5(word,l):
+    last = len(l)-1
+    start = 0
+    index = (start+last)//2
+    switch = 1
+    befstart,beflast = "",""
+    while l[index]!=word:
+        #print(index,end=" ")
+        if l[index]=="":
+            index -= 1*switch
+        else:#value 자체가 리스트에 존재하지 않는경우는?
+            if last==beflast and start==befstart:
+                return "Not Exist"
+            if l[index]<word:
+                befstart = start
+                start = index
+                index = (last+start)//2
+                switch *= -1
+            else:
+                beflast = last
+                last = index
+                index = (last+start)//2
+                switch *= -1
+    return index
+print("func5",end = " ")
+print(func5("abc",["abc","","","","ball","","","carry"]))
+
+def func6(string,longs = 200000000000):
+    #1GB씩 16개로 나눠서 각각을 정렬한후, 정렬된 문자쌍 두개를 서로 작은것부터 저장(mergesort하듯이) 
+    #하지만 이렇게 하면서 메모리에 초과될수 있으니 정렬된 문자쌍에서 625000000(625MB)씩 먼저 정렬하고 한 쌍이 다 사용되면 꼬리쌍을 꺼내서 정렬한다
+    #한쪽이 기존에 정렬된 쌍이 다 정렬될 경우 나머지 부분을 붙여서 save
+    #합치면서 합쳐진 값이 1GB가 넘어가면 우선적으로 저장하고 추가적으로 실행
+    #20GB = 200000000000Byte = 4 * 4 * 1250000000(AScii)
+    #근데 전부 global 선언해버리면 공간 낭비 개심한데 어떻게 로컬로 적정하고할당해주지...?
+
+    if longs%32 != 0:
+        return "Only multiples of 32" 
+    long = longs//16
+    for i in range(15):
+        s = string[i*long:(i+1)*long]
+        globals()["string{}".format(i)] = "".join(map(chr,merge_sort(list(map(ord,s)))))
+    else:
+        s = string[long*15:]
+        globals()["string{}".format(15)] = "".join(map(chr,merge_sort(list(map(ord,s)))))
+    k = 8
+    while k!=0:
+        for i in range(k):
+            a = locals()["string"+str(2*i)]
+            b = locals()["string"+str(2*i+1)]
+            destination = 16//k
+            #print(a)
+            #print(b)
+            
+            for n in range(16//k):
+                globals()["a{}".format(n)] = a[(long//2)*n:(long//2)*(n+1)]
+                globals()["b{}".format(n)] = b[(long//2)*n:(long//2)*(n+1)]
+            astart = 0
+            bstart = 0
+            aindex = 0
+            bindex = 0
+            temp = ""
+            sol = ""
+            while astart!=destination and bstart!=destination:
+                if aindex!=long//2 and bindex!=long//2:
+                    if (globals()["a{}".format(astart)])[aindex]<(globals()["b{}".format(bstart)])[bindex]:
+                        temp += (globals()["a{}".format(astart)])[aindex]
+                        aindex += 1
+                    else:
+                        temp += (globals()["b{}".format(bstart)])[bindex]
+                        bindex += 1
+                else:
+                    sol += temp
+                    temp = ""
+                    if aindex==long//2:
+                        aindex = 0
+                        astart += 1
+                    if bindex==long//2:
+                        bindex = 0
+                        bstart +=1
+            if astart==destination and aindex==0:
+                while bstart!=destination:
+                    sol += globals()["b{}".format(bstart)][bindex:]
+                    bindex = 0
+                    bstart += 1
+            else:
+                while astart!=destination:
+                    sol += globals()["a{}".format(astart)][aindex:]
+                    aindex = 0
+                    astart += 1
+            globals()["string{}".format(i)] = sol
+        k //= 2
+    return globals()["string{}".format(0)]
+
+def func7(l,lastnumber = 2**33-1):
+    #음이 아닌 정수 40억 개로 이루어진 입력파일이 있다. 이 파일에 없는 정수를 생성하는 알고리즘을 작성하라.(unsigned int)
+    #단 메모리는 1GB만들 사용할수 있다.
+    #int하나는 32bit라고 한다면, 4byte. 4,000,000,000  * 4 = 16 GB
+    #16 = 2*8 = 2*2*4 = 2*2*2*2 = (2**5)*.5
+    longs = len(l)
+    long = longs//32
+    for i in range(31):
+        globals()["numbers{}".format(i)] = merge_sort(l[long*i:long*(i+1)])
+    else:
+        globals()["numbers{}".format(31)] = merge_sort(l[long*31:])
+    indexs = [0]*32
+    notAppear = []
+    for n in range(lastnumber+1):
+        for i in range(len(indexs)):
+            if indexs[i]<len(globals()["numbers{}".format(i)]):
+                if n == globals()["numbers{}".format(i)][indexs[i]]:
+                    indexs[i] += 1
+                    break
+        else:
+            notAppear.append(n)
+    notAppear.sort()
+    return notAppear
+
+
+def fun8(l):
+    #1부터 N(<=32000)까지의 숫자로 이루어진 배열이 있다. 배열엔 중복된 숫자가 나타날 수 있고, N이 무엇인지 알수 없다.
+    #사용 가능한 메모리가 4KB일때, 중복된 원소를 모두 출력하려면 어떻게 할수있을까
+    #4KB = 4000Byte = 32000 bit
+    #즉 각 비트 index를 수라고 보고 크기가 32000인 bit array를 만든다
+    numbers = [False]*32000 # l = bit[32001]
+    for i in range(len(l)):
+        num = l[i]
+        if numbers[num-1] == False:
+            numbers[num-1] = True
+    sol = []
+    for i in range(len(numbers)):
+        if numbers[i]==False:
+            sol.append(i)
+    return sol
+
+def func9(l,number = 0):
+    #이 문제가 값이 하나밖에 존재 하지 않는다고 가정!
+    m = len(l[0])
+    n = len(l)
+    RS,CS = 0,0#(rowstart,columnstart)
+    RL,CL = n-1,m-1
+    while True:
+        #print(f"RS={RS},RL={RL},CS={CS},CL={CL}")
+        if RS==RL and CS==CL:
+            print(l[RS][CS])
+            return (RS,CS)
+        if not(l[RS][CS]<=number and number<=l[RS][CL]) and RS<RL:
+            RS += 1
+            continue
+        if not(l[RS][CS]<=number and number<=l[RL][CS]) and CS<CL:
+            CS += 1
+            continue
+        if not(l[RL][CS]<=number and number<=l[RL][CL]) and RS<RL:
+            RL -= 1
+            continue
+        if not(l[RS][CL]<=number and number<=l[RL][CL]) and CS<CL:
+            CL -= 1
+            continue
+        break
+    return "Can\'t Find"
+
+
+#11번
+#list로 구현한다고 하면,
+#그래도 모르겟는데요ㅠ
+# class Node():
+#     def __init__(self,data = []):
+#         self.data = data
+#         self.long = 0
+#     def track(self,data):
         
+
+# stream = Node()
+# for i in range([5,1,4,4,5,9,7,13,3]):
+#     stream.track(i)
+
+
+
+def func11(l):
+    #가장 simple
+    #sorting 해서 max,min순으로 계속해서 나옴
+    #but 동치들이 많을때 에러가 뜰수있음
+    #큰거순, 작은거순으로 해보고 True,False로 감소 증가 판단해서 하면 될거같긴함...
+    #근데 이거로 답이 안나오는데, 존재하는 답이 없다는 보장을 할수가 있는가?
+    #그건 모르겟네여ㅠㅠㅠ
+    #글고 이 문제가 요구하는 게 한가지 케이스만 출력하라는 건지,
+    #아니면 모든 케이스를 다출력하라는 건지(근데 출력기준으로 이건 아닌거같음)
+    s_l = merge_sort(l)
+    switch = True
+    check = True
+    seq = []
+    for _ in range(len(l)):
+        if switch==True:
+            value,s_l = s_l[-1],s_l[:-1]
+            seq.append(value)
+            switch = False
+        else:
+            value,s_l = s_l[0],s_l[1:]
+            seq.append(value)
+            switch = True
+    switch = 1 #첫 값은 감소
+    index = 1
+    while index!=len(l):
+        if switch*(seq[index]-seq[index-1])<0:
+            index,switch = index+1,switch*(-1)
+        else:
+            check = False
+            break
+    if check==False:
+        s_l = merge_sort(l)
+        switch = False
+        check = True
+        seq = []
+        for _ in range(len(l)):
+            if switch==True:
+                value,s_l = s_l[-1],s_l[:-1]
+                seq.append(value)
+                switch = False
+            else:
+                value,s_l = s_l[0],s_l[1:]
+                seq.append(value)
+                switch = True
+        switch = -1 #첫 값은 증가
+        index = 1
+        while index!=len(l):
+            if switch*(seq[index]-seq[index-1])<0:
+                index,switch = index+1,switch*(-1)
+            else:
+                check = False
+                break
+    if check==False:
+        return "can't find"
+    else:
+        return seq
+        
+    
+
+
+
 
 #print(func1([1,3,7,10],[2,5,8,9]))
 #print(func3([15,16,19,20,25,1,3,4,5,7,10,14],5))
 #a = Listy([1,1,1,1,2,2,2,3,4,4,4,4,4,5,6,7,9,10])
 #print(f"listy{func4(a,5)}")
 #print(func4(a,7))
-strings = ["abc","bac","cab","adb","adv","cdv","dvc"]
-print(func2(strings))
+#strings = ["abc","bac","cab","adb","adv","cdv","dvc"]
+#print(func2(strings))
+#print(func6("nkgdeochifplambjabcdefghijklmnop",32))
+#print("".join(sorted("nkgdeochifplambjabcdefghijklmnop")))
+import random
+permutation2 = list(range(10))
+permutation2.remove(4)
+permutation2.remove(8)
+permutation = list(range(65))
+permutation.remove(32)
+permutation.remove(64)
+random.shuffle(permutation)
+random.shuffle(permutation2)
+# for i in [1,6,11,16,3,8,12,19,5,9,14,22,7,10,15,23]:
+    # print(func9([[1,6,11,16],[3,8,12,19],[5,9,14,22],[7,10,15,23]],i))
+print("func11",end = "  ")
+print(func11([1,3,3,3,5]))
+
 
 import unittest
 class Test(unittest.TestCase):
@@ -215,5 +466,7 @@ class Test(unittest.TestCase):
     self.assertEqual(func4(a,9),3)
     self.assertEqual(func4(a,10),4)
     self.assertEqual(func4(a,11),False)
+    self.assertEqual(func7(permutation,64),[32,64])
+    self.assertEqual(func7(permutation2,9),[4,8])
 if __name__ == "__main__":
     unittest.main()
